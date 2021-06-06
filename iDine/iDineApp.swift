cbcc275e115ef51da184ff3c87e1691bf4e13b2a
gsandvoss <gsandvoss@gmail.com>
2021-06-05 17:34:11 -0700

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by glenn sandvoss on 6/5/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
